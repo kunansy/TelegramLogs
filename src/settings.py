23b2a28bbb6bd9c013df83d6412168f5997697b2
kunansy <kolobov.kirill@list.ru>
2021-09-06 16:05:50 +0300

    settings updated
    GRANTED_USERS added

--- src/settings.py
+++ src/settings.py
@@ -4,5 +4,7 @@ from environs import Env
 env = Env()
 env.read_env('.env')
 
+GRANTED_USERS = env.list('GRANTED_USERS', [])
+
 with env.prefixed('TELEGRAM_BOT_'):
     TELEGRAM_BOT_TOKEN = env('TOKEN')
