1ae6a81f4f0820656ee8edf2bde7d6d9210ba1fc
kunansy <kolobov.kirill@list.ru>
2021-09-06 17:19:16 +0300

    settings updated
    DEVELOPER_USERNAME addded

--- src/settings.py
+++ src/settings.py
@@ -5,6 +5,7 @@ env = Env()
 env.read_env('.env')
 
 GRANTED_USERS = env.list('GRANTED_USERS', [])
+DEVELOPER_USERNAME = env('DEVELOPER_USERNAME')
 
 with env.prefixed('TELEGRAM_BOT_'):
     TELEGRAM_BOT_TOKEN = env('TOKEN')
