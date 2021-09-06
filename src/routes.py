bf4a95803a63957d1ca2928a3396f77b51af0bf9
kunansy <kolobov.kirill@list.ru>
2021-09-06 17:20:24 +0300

    routes improved

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,15 @@
+import asyncio
 from functools import partial
 
-from aiogram import Bot, Dispatcher, types, executor
-from src import settings
+from aiogram import Bot, Dispatcher, executor, types
 
+from src import settings, mesages_listener
+
+
+loop = asyncio.get_event_loop()
 
 bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, loop=loop)
 
 allow_access = lambda msg: msg.from_user.username in settings.GRANTED_USERS
 message_handler = partial(dp.message_handler, allow_access)
@@ -23,4 +27,5 @@ async def echo(msg: types.Message) -> None:
 
 
 def main() -> None:
+    dp.loop.create_task(mesages_listener.socket_listener())
     executor.start_polling(dp, skip_updates=True)
