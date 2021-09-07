b73fc5c9dde3ee8ca121d6eb0b938f5767342f94
kunansy <kolobov.kirill@list.ru>
2021-09-07 10:02:26 +0300

    settings added

--- src/settings.py
+++ src/settings.py
@@ -4,8 +4,13 @@ from environs import Env
 env = Env()
 env.read_env('.env')
 
+# username of people who can access the bot
 GRANTED_USERS = env.list('GRANTED_USERS', [])
 DEVELOPER_USERNAME = env('DEVELOPER_USERNAME')
 
 with env.prefixed('TELEGRAM_BOT_'):
     TELEGRAM_BOT_TOKEN = env('TOKEN')
+    TELEGRAM_BOT_CHAT_ID = env.int('CHAT_ID')
+
+LOG_MSG_HANDLER_HOST = '127.0.0.1'
+LOG_MSG_HANDLER_PORT = 2025
