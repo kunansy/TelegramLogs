04339d2db2fab504f703436a1c46a40ba8b1eb96
kunansy <kolobov.kirill@list.ru>
2021-09-07 12:35:26 +0300

    auth() implemented

--- src/bot_api.py
+++ src/bot_api.py
@@ -1,5 +1,6 @@
 import asyncio
-from functools import partial
+from functools import wraps
+from typing import Callable
 
 from aiogram import Bot, Dispatcher, types
 
@@ -11,8 +12,14 @@ loop = asyncio.get_event_loop()
 bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
 dp = Dispatcher(bot, loop=loop)
 
-allow_access = lambda msg: msg.from_user.username in settings.GRANTED_USERS
-message_handler = partial(dp.message_handler, allow_access)
+
+def auth(func: Callable):
+    @wraps(func)
+    async def wrapped(msg: types.Message):
+        if msg.from_user.username not in settings.GRANTED_USERS:
+            return await msg.reply("I don't fucking know who are you, man", reply=False)
+        return await func(msg)
+    return wrapped
 
 
 async def send_log_record(log_record: _log_record.LogRecord) -> None:
@@ -20,12 +27,14 @@ async def send_log_record(log_record: _log_record.LogRecord) -> None:
         settings.TELEGRAM_BOT_CHAT_ID, text)
 
 
-@message_handler(commands=['start'])
+@dp.message_handler(commands=['start'])
+@auth
 async def welcome(msg: types.Message) -> None:
     await msg.reply(f"Hi there!\nI'm logs handler bot!\n"
                     f"Developed by @{settings.DEVELOPER_USERNAME}")
 
 
-@message_handler()
+@dp.message_handler()
+@auth
 async def echo(msg: types.Message) -> None:
     await msg.answer(msg.text)
