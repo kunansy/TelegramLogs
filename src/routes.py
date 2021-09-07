e578f06978fde0b10134915d8aed3c0200a1851d
kunansy <kolobov.kirill@list.ru>
2021-09-07 10:02:07 +0300

    routes fixed

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,9 @@
 import asyncio
 from functools import partial
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
 
-from src import settings, mesages_listener
+from src import settings
 
 
 loop = asyncio.get_event_loop()
@@ -15,6 +15,11 @@ allow_access = lambda msg: msg.from_user.username in settings.GRANTED_USERS
 message_handler = partial(dp.message_handler, allow_access)
 
 
+async def send_message(text: str) -> None:
+    await bot.send_message(
+        settings.TELEGRAM_BOT_CHAT_ID, text)
+
+
 @message_handler(commands=['start'])
 async def welcome(msg: types.Message) -> None:
     await msg.reply(f"Hi there!\nI'm logs handler bot!\n"
@@ -24,8 +29,3 @@ async def welcome(msg: types.Message) -> None:
 @message_handler()
 async def echo(msg: types.Message) -> None:
     await msg.answer(msg.text)
-
-
-def main() -> None:
-    dp.loop.create_task(mesages_listener.socket_listener())
-    executor.start_polling(dp, skip_updates=True)
