6d5e65970d37f8b0b9fd6820e4e44e642e46c5e3
kunansy <kolobov.kirill@list.ru>
2021-09-07 10:05:57 +0300

    main file fixed

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-from src import routes
+
+from aiogram import executor
+
+from src import msg_handler
+from src.bot_api import dp
+
+
+def main() -> None:
+    dp.loop.create_task(msg_handler.start_app())
+    executor.start_polling(dp, skip_updates=True)
 
 
 if __name__ == '__main__':
