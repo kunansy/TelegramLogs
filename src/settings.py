c9c72383dc24b60b3e31778dd8ec152308d2ea89
kunansy <kolobov.kirill@list.ru>
2021-09-07 11:03:43 +0300

    logger level added to settings

--- src/settings.py
+++ src/settings.py
@@ -7,9 +7,11 @@ env.read_env('.env')
 # username of people who can access the bot
 GRANTED_USERS = env.list('GRANTED_USERS', [])
 DEVELOPER_USERNAME = env('DEVELOPER_USERNAME')
+LOGGER_LEVEL = env.log_level('LOGGER_LEVEL', 'DEBUG')
 
 with env.prefixed('TELEGRAM_BOT_'):
     TELEGRAM_BOT_TOKEN = env('TOKEN')
+    # TODO: ContextVar or db or another thing
     TELEGRAM_BOT_CHAT_ID = env.int('CHAT_ID')
 
 LOG_MSG_HANDLER_HOST = '127.0.0.1'
