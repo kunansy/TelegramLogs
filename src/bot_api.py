56a29f3d6efc9816208e6286d4c725fd8a31fb5e
kunansy <kolobov.kirill@list.ru>
2021-09-08 13:15:41 +0300

    send_log_record processes all exceptions

--- src/bot_api.py
+++ src/bot_api.py
@@ -3,9 +3,10 @@ from functools import wraps
 from typing import Callable
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.utils import exceptions as bex
 
 from src import _log_record, settings
-
+from src.log import logger
 
 loop = asyncio.get_event_loop()
 
@@ -24,10 +25,26 @@ def auth(func: Callable):
 
 async def send_log_record(log_record: _log_record.LogRecord) -> None:
     for chat_id in settings.TELEGRAM_BOT_CHAT_IDS:
-        await bot.send_message(
-            chat_id, log_record.format(),
-            parse_mode="markdown", disable_web_page_preview=True
-        )
+        try:
+            await bot.send_message(
+                chat_id, log_record.format(),
+                parse_mode="markdown", disable_web_page_preview=True
+            )
+        except bex.BotBlocked:
+            logger.error("Bot blocked")
+        except bex.ChatNotFound:
+            logger.error("Char not found")
+        except bex.RetryAfter as e:
+            logger.error(f"Target [ID:{chat_id}]: Flood limit is exceeded. "
+                        f"Sleep {e.timeout} seconds.")
+            await asyncio.sleep(e.timeout)
+            return await send_log_record(log_record)
+        except bex.UserDeactivated:
+            logger.error(f"Target [ID:{chat_id}]: user is deactivated")
+        except bex.TelegramAPIError:
+            logger.exception(f"Target [ID:{chat_id}]: failed")
+        else:
+            logger.info(f"Target [ID:{chat_id}]: success")
 
 
 @dp.message_handler(commands=['start'])
