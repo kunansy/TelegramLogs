67b5717db8814e19b7640800df4bd2ccacbe93ce
kunansy <kolobov.kirill@list.ru>
2021-09-09 11:54:01 +0300

    on_startup added

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 from aiogram import executor
 
 from src import msg_handler
-from src.bot_api import dp
+from src import bot_api
+
+
+async def on_startup(*args):
+    await bot_api.set_default_commands()
+    await bot_api.notify_startup()
 
 
 def main() -> None:
-    dp.loop.create_task(msg_handler.start_app())
-    executor.start_polling(dp, skip_updates=True)
+    bot_api.dp.loop.create_task(msg_handler.start_app())
+    executor.start_polling(bot_api.dp, on_startup=on_startup)
 
 
 if __name__ == '__main__':
